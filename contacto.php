12d4ff4b76f465fdc668ab78786cdbedad667fad
uziel111 <uziel.cap.123gmail.com>
2023-06-21 14:50:48 -0600

    ajuste contacto

--- contacto.php
+++ contacto.php
@@ -132,7 +132,7 @@
 														</a>
 													</li>
 													<li>
-														<a class="nav-link" href="contacto.html">
+														<a class="nav-link" href="contacto.php">
 															Contacto
 														</a>
 													</li>
